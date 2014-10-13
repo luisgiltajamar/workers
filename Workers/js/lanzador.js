bf855d2c8c2577c1e97ee95f971a726f073c6f55
luisgiltajamar <luis.gil@tajamar365.com>
2014-10-13 09:52:32 +0200

    WebWorkers

---
+++ Workers/js/lanzador.js
@@ -0,0 +1,24 @@
+﻿var trabajador;
+
+function iniciar() {
+
+    if (typeof (Worker)!=="undefined") {
+        trabajador = new Worker("js/workerReloj.js");
+
+    }
+    trabajador.onmessage = function(evt) {
+        document.getElementById("reloj").innerHTML = evt.data;
+
+    };
+    trabajador.onerror = function(err) {
+        alert(err.data);
+
+    };
+
+}
+
+(function() {
+
+    iniciar();
+
+})();
---
+++ Workers/js/workerReloj.js
@@ -0,0 +1,11 @@
+﻿function calculaHora() {
+    var hora = new Date();
+
+    var horaTexto = hora.getHours() + ":" + hora.getMinutes() + ":" + hora.getSeconds();
+
+    postMessage(horaTexto);
+    setTimeout("calculaHora()", 1000);
+
+}
+
+calculaHora();
